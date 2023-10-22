e97b91d53153c97eff27408630af61e9884383cb
DaniloSTFR <danilo.santosfr@gmail.com>
2023-10-21 21:01:18 -0300

    Conclusão do Desafio Dotnet POO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,33 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+        }
+
+        public void DatelhesSmartphone(){
+            Console.WriteLine($"Detalhes do {Modelo}:"
+                    +$"\n\tNumero: {Numero}"
+                    +$"\n\tMemoria: {Memoria}"
+                    +$"\n\tIMEI: {IMEI}");
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando {Modelo}...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação no {Modelo}...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
